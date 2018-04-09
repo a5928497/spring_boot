786d82be4fa4b0c535c5184d38083d878110a1e0
a5928497 <61722615@qq.com>
2018-04-09 11:28:42 +0800

    User entity add password col

--- src/main/java/com/lzl/springboot_crud/entity/User.java
+++ src/main/java/com/lzl/springboot_crud/entity/User.java
@@ -64,6 +64,14 @@ public class User {
         this.createTime = createTime;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
         return "User{" +
