7e1c91fc668413e62e642cc8ca01eb634d4af6fc
a5928497 <61722615@qq.com>
2018-04-11 00:08:35 +0800

    create login controller with shiro

--- src/main/java/com/lzl/springboot_crud/entity/Permission.java
+++ src/main/java/com/lzl/springboot_crud/entity/Permission.java
@@ -1,7 +1,40 @@
 package com.lzl.springboot_crud.entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Permission {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Integer id;
+    private String permission;
+
+    @JoinColumn(name = "ROLE_ID")
+    @ManyToOne
+    private Role role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
