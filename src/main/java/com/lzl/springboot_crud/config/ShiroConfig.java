7f4f47187b5f3858f8c975f791939e965bf9635f
a5928497 <61722615@qq.com>
2018-04-11 21:52:01 +0800

    creating loginRealm

--- src/main/java/com/lzl/springboot_crud/config/ShiroConfig.java
+++ src/main/java/com/lzl/springboot_crud/config/ShiroConfig.java
@@ -4,53 +4,54 @@ import com.lzl.springboot_crud.realm.UserRealm;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedHashMap;
 
 @Configuration
 public class ShiroConfig {
-
-    //将realm加入容器
     @Bean
+    public ShiroFilterFactoryBean (@Qualifier("securityManager") SecurityManager manager){
+        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+        bean.setSecurityManager(manager);
+        //配置登录的url和登录成功的url
+        bean.setLoginUrl("/login");
+        bean.setSuccessUrl("/users");
+        //配置访问权限
+        LinkedHashMap<String, String> filterChainDefinitionMap=new LinkedHashMap<>();
+        filterChainDefinitionMap.put("/jsp/login.jsp*", "anon"); //表示可以匿名访问
+        filterChainDefinitionMap.put("/loginUser", "anon");
+        filterChainDefinitionMap.put("/logout*","anon");
+        filterChainDefinitionMap.put("/jsp/error.jsp*","anon");
+        filterChainDefinitionMap.put("/jsp/index.jsp*","authc");
+        filterChainDefinitionMap.put("/*", "authc");//表示需要认证才可以访问
+        filterChainDefinitionMap.put("/**", "authc");//表示需要认证才可以访问
+        filterChainDefinitionMap.put("/*.*", "authc");
+        bean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return bean;
+    }
+
+    //配置核心安全事务管理器
+    @Bean(name="securityManager")
+    public DefaultWebSecurityManager securityManager(@Qualifier("userRealm") UserRealm userRealm) {
+        System.err.println("--------------shiro已经加载----------------");
+        DefaultWebSecurityManager manager=new DefaultWebSecurityManager();
+        manager.setRealm(userRealm);
+        return manager;
+    }
+
+    //配置自定义的权限登录器
+    @Bean(name="userRealm")
     public UserRealm userRealm() {
         UserRealm userRealm = new UserRealm();
-        return  userRealm;
-    }
-
-    //权限管理，配置主要是Realm的管理认证，在securityManager中加入自己写的realm
-    @Bean
-    public DefaultWebSecurityManager securityManager() {
-        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        securityManager.setRealm(userRealm());
-        return securityManager;
-    }
-
-    //Filter工厂，设置对应的过滤条件和跳转条件
-    @Bean
-    public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager securityManager) {
-        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-        shiroFilterFactoryBean.setSecurityManager(securityManager);
-        Map<String,String> map = new HashMap<String, String>();
-        //登出
-        map.put("/logout","logout");
-        //对所有用户认证
-        map.put("/**","authc");
-        //登录
-        shiroFilterFactoryBean.setLoginUrl("/login");
-        //首页
-        shiroFilterFactoryBean.setSuccessUrl("/index");
-        //错误页面，认证不通过跳转
-        shiroFilterFactoryBean.setUnauthorizedUrl("/error/4xx");
-        shiroFilterFactoryBean.setFilterChainDefinitionMap(map);
-        return shiroFilterFactoryBean;
+        return userRealm;
     }
 
     //加入注解的使用，不加入这个注解不生效
     @Bean
-    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(DefaultWebSecurityManager securityManager) {
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
         AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
         authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
         return authorizationAttributeSourceAdvisor;
