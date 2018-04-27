d180b6915fe26474129359934ec8e90b374073ad
a5928497 <61722615@qq.com>
2018-04-27 15:13:57 +0800

    connected to redis server

--- src/test/java/com/lzl/springboot_crud/SpringbootCrudApplicationTests.java
+++ src/test/java/com/lzl/springboot_crud/SpringbootCrudApplicationTests.java
@@ -6,14 +6,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootCrudApplicationTests {
 
 	@Test
-	public void contextLoads() {
-
+	public void redisTest() {
+		Jedis jedis = new Jedis("192.168.88.246",6379);
+		System.out.println(jedis.ping());
 	}
 
 }
